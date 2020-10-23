089754d6dd397e7b911cb07691176dbb3464c9ae
ishiDACo <2542762+ishiDACo@users.noreply.github.com>
2020-10-23 22:12:57 +0900

    [#121] Support redhat_api
    - Add `redhat_api`
    - Refactor CSS for `CVSS Severity`
    - Change `CweID` source
    - Change `CVSS Severity` source
    - Change `CVSS Severity` sort

--- dist/js/vulsrepo_common.js
+++ dist/js/vulsrepo_common.js
@@ -588,7 +588,7 @@ const isHelpHTMLScore = function() {
                     <td>A fix has been applied and updated packages are awaiting arrival into the archive. This often used when a package is received into -proposed for wider testing.</td>
                 </tr>
                 <tr>
-                    <td class="cvss-NotVulnerable">Not Vulnerable</td>
+                    <td class="cvss-Notvulnerable">Not Vulnerable</td>
                     <td>Packages which do not exist (DNE) in the archive, are not affected by the vulnerability or have a fix applied in the archive.</td>
                 </tr>
             </tbody>
@@ -603,19 +603,19 @@ const isHelpHTMLScore = function() {
                     <th>Score</th>
                 </tr>
                 <tr>
-                    <td class="cvss-high">High</td>
+                    <td class="cvss-High">High</td>
                     <td>A typical, exploitable security problem, which you'll really like to fix or at least implement a workaround. This could be because the vulnerable code is very broadly used, because an exploit is in the wild or because the attack vector is very wide. Should be put into that category anything that permits an attacker to execute arbitrary code on the vulnerable system (with or without root privileges) and high-impact denial-of-service bugs (for instance, an IPv4 forwarding path vulnerability which requires only very few packets to exploit). Significant defects in security software can be rated "high" as well (for instance, a vulnerability in a piece of cryptographic software which flags forged digital signatures as genuine).</td>
                 </tr>
                 <tr>
-                    <td class="cvss-medium">Medium</td>
+                    <td class="cvss-Medium">Medium</td>
                     <td>For anything which permits code execution after user interaction. Local privilege escalation vulnerabilities are in this category as well, or remote privilege escalation if it's constrained to the application (i.e., no shell access to the underlying system, such as simple cross-site scripting). Most remote DoS vulnerabilities fall into this category, too.</td>
                 </tr>
                 <tr>
-                    <td class="cvss-low">Low</td>
+                    <td class="cvss-Low">Low</td>
                     <td>A security problem, which has only mild security implications (local DoS, /tmp file races and so on).</td>
                 </tr>
                 <tr>
-                    <td class="cvss-unimportant">Unimportant</td>
+                    <td class="cvss-Unimportant">Unimportant</td>
                     <td>This problem does not affect the Debian binary package, e.g., a vulnerable source file, which is not built, a vulnerable file in doc/foo/examples/, PHP Safe mode bugs, path disclosure (doesn't matter on Debian). All "non-issues in practice" fall also into this category, like issues only "exploitable" if the code in question is setuid root, exploits which only work if someone already has administrative privileges or similar. This severity is also used for vulnerabilities in packages which are not covered by security support.</td>
                 </tr>
             </tbody>
